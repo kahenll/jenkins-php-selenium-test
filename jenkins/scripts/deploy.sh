a1e156a92d51de4bafaba4b88ad5137ff89da271
kahenll <2002841@sit.singaporetech.edu.sg>
2022-11-16 19:47:49 +0800

    fix publish

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 5000:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test/src:/var/www/html --net test4net php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v $PWD/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:5000 to see your PHP application in action.'
+echo 'Visit http://localhost:80 to see your PHP application in action.'
